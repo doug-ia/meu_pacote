73d0c5471895e93f919a45afa035528f1af024ed
Doug-ia <trindademateus120@gmail.com>
2025-04-05 08:50:00 -0300

    quarto commit

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="meu_pacote",  # Nome do pacote no PyPI
     version="0.0.1",
-    author="Jonatan Mateus",
+    author="doug.ia",
     author_email="seuemail@example.com",
     description="Um pacote simples para dizer olá",
-    long_description=open("README.md", encoding="utf-8").read(),
+    long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/seuusuario/meu_pacote",  # Seu repositório
+    url="https://github.com/doug-ia/meu_pacote.git",  # Seu repositório
     packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
+    install_requires = requirements,
+    python_requires=">=3.8",
 )
